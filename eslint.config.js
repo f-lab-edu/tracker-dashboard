bc42d73857b697ac224249d2abece4da51d7585a
임재원 <nemo0824@naver.com>
2025-03-14 16:54:22 +0900

    feature: login 기능 생성

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,75 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
-import prettier from 'eslint-config-prettier';
+// import js from '@eslint/js';
+// import {
+//   default as eslintConfigPrettier,
+//   default as prettier,
+// } from 'eslint-config-prettier';
+// import eslintPluginPrettier from 'eslint-plugin-prettier';
+// import reactHooks from 'eslint-plugin-react-hooks';
+// import reactRefresh from 'eslint-plugin-react-refresh';
+// import globals from 'globals';
+// import tseslint from 'typescript-eslint';
+// import eslintPluginReact from 'eslint-plugin-react';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+// export default tseslint.config(
+//   { ignores: ['dist'] },
+//   {
+//     extends: [
+//       js.configs.recommended,
+//       ...tseslint.configs.recommended,
+//       prettier,
+//       'plugin:prettier/recommended',
+//       'plugin:react/recommended',
+//       eslintConfigPrettier,
+//     ],
+//     files: ['**/*.{ts,tsx}'],
+//     languageOptions: {
+//       ecmaVersion: 2020,
+//       globals: globals.browser,
+//     },
+//     plugins: {
+//       'react-hooks': reactHooks,
+//       'react-refresh': reactRefresh,
+//       react: eslintPluginReact,
+//       prettier: eslintPluginPrettier,
+//     },
+//     rules: {
+//       ...reactHooks.configs.recommended.rules,
+//       ...eslintPluginReact.configs.recommended.rules,
+//       ...tseslint.configs.recommended.rules,
+//       ...eslintConfigPrettier.rules,
+//       'prettier/prettier': 'error',
+//       'react-refresh/only-export-components': [
+//         'warn',
+//         { allowConstantExport: true },
+//       ],
+//     },
+//   }
+// );
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintPluginReact from 'eslint-plugin-react';
+
+export default [
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      prettier,
-    ],
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: tsparser,
+      sourceType: 'module',
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      react: eslintPluginReact,
+      '@typescript-eslint': tseslint,
+      prettier: eslintPluginPrettier,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      ...eslintPluginReact.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      ...eslintConfigPrettier.rules,
+      'prettier/prettier': ['error', { semi: true }],
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
     },
-  }
-);
+  },
+];
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,9 @@
+import { AuthLayout } from '@/components/auth/AuthLayout';
+
+export const LoginPage = () => {
+  return (
+    <>
+      <AuthLayout></AuthLayout>
+    </>
+  );
+};
