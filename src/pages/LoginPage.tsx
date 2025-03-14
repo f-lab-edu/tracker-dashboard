ab0c54f571ba95960187677bbf3f876bd0b842db
임재원 <nemo0824@naver.com>
2025-03-15 02:18:50 +0900

    chore: api 수정, <></>삭제, 주석제거.

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,9 +1,5 @@
 import { AuthLayout } from '@/components/auth/AuthLayout';
 
 export const LoginPage = () => {
-  return (
-    <>
-      <AuthLayout></AuthLayout>
-    </>
-  );
+  return <AuthLayout></AuthLayout>;
 };
