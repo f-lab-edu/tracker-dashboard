33388423d06920dc3107e1d46f452f84a9178c96
임재원 <nemo0824@naver.com>
2025-03-20 02:58:42 +0900

    refactor: ci에러 수정 component: ()=>{}에 컴포넌트 반환하도록 분리

--- src/routes/_authenticated.tsx
+++ src/routes/_authenticated.tsx
@@ -2,25 +2,27 @@ import { getUserSession } from '@/lib/auth';
 import { createFileRoute, Navigate, Outlet } from '@tanstack/react-router';
 import { useEffect, useState } from 'react';
 
+const Authenticated = () => {
+  const [user, setUser] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const userSession = await getUserSession();
+      setUser(userSession);
+      setLoading(false);
+    })();
+  }, []);
+
+  if (isLoading === true) {
+    return <p>로딩중...</p>;
+  }
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+  return <Outlet />;
+};
+
 export const Route = createFileRoute('/_authenticated')({
-  component: () => {
-    const [user, setUser] = useState(null);
-    const [isLoading, setLoading] = useState(true);
-
-    useEffect(() => {
-      (async () => {
-        const userSession = await getUserSession();
-        setUser(userSession);
-        setLoading(false);
-      })();
-    }, []);
-
-    if (isLoading === true) {
-      return <p>로딩중...</p>;
-    }
-    if (!user) {
-      return <Navigate to="/login" />;
-    }
-    return <Outlet />;
-  },
+  component: () => <Authenticated />,
 });
