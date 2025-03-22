ba4bad90d35c5c0a8eced731344ca06fe82fae54
임재원 <nemo0824@naver.com>
2025-03-22 23:29:26 +0900

    fix: gitignore 수정, 이미지 타입추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react-swc';
-import fs from 'fs';
 import * as path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -18,13 +13,5 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
-  },
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
-    },
-    host: 'localhost',
-    port: 5173,
   },
 });
