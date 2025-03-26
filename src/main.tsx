a8b0bd37d04f6647e80c71612d8df9a13d5410d5
임재원 <nemo0824@naver.com>
2025-03-27 03:51:34 +0900

    chore: react-query, rechart 설치 및 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRouter, RouterProvider } from '@tanstack/react-router';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
@@ -12,8 +13,12 @@ declare module '@tanstack/react-router' {
   }
 }
 
+const queryClient: QueryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 );
