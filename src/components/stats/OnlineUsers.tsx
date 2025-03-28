04d5eb399156d143c6c697e915d27c76cf4d68d9
임재원 <nemo0824@naver.com>
2025-03-28 15:12:49 +0900

    feature: OnlineUsers 컴포넌트 폴링사용.

---
+++ src/components/stats/OnlineUsers.tsx
@@ -0,0 +1,29 @@
+import { getAxiosData } from '@/utils/api';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { MdOnlinePrediction } from 'react-icons/md';
+import { Card } from '../common/Card';
+
+interface OnlineUsersDataType {
+  onlineUsersCount: number;
+}
+
+export const OnlineUsers = () => {
+  const { data } = useSuspenseQuery<OnlineUsersDataType>({
+    queryKey: ['onlineUser'],
+    queryFn: () => getAxiosData('/dashboard/onlineUsersCount'),
+    refetchInterval: 10000,
+    staleTime: 0,
+  });
+
+  return (
+    <Card borderRadius="xl" width="fit">
+      <div>
+        <p className="text-lg font-bold">Online User Count</p>
+        <div className="flex gap-4 items-center justify-center mt-4">
+          <MdOnlinePrediction size={30} />
+          <p className="text-center text-3xl">{data.onlineUsersCount}</p>
+        </div>
+      </div>
+    </Card>
+  );
+};
