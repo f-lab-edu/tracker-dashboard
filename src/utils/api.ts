00ca1d61b807740cff98f7ab3766741836c94c32
임재원 <nemo0824@naver.com>
2025-04-01 13:09:21 +0900

    refactor: fetchData, params추가

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,9 +1,13 @@
 import { API_BASE_URL } from '@/config/api';
 import axios from 'axios';
 
-export const fetchData = async (url: string) => {
+export const fetchData = async (
+  url: string,
+  params?: Record<string, string>
+) => {
   const res = await axios.get(`${API_BASE_URL}${url}`, {
     withCredentials: true,
+    params,
   });
   return res.data;
 };
