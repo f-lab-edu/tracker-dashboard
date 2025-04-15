96e697950559a117d8dadc4094dcb778d361b103
임재원 <nemo0824@naver.com>
2025-04-15 15:01:58 +0900

    refactor: ci/cd 변경 aws 경로 무효화 추가

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [
     TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
     react(),
